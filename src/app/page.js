76b979d0b12a008a419d80a689703e420558e65e
karan-pandey15 <pandeykaran1515@gmail.com>
2024-10-27 18:07:49 +0530

    code push to git

---
+++ src/app/page.js
@@ -0,0 +1,29 @@
+ 
+import Category from "./components/Category/page";
+import Banner1 from "./components/homepage/page";
+import Footer from "./components/footer/page"; 
+import Navbar from "./components/navbar/page";    
+import AllProduct from "./pages/allproducts/page";
+import "./globals.css"
+export default function Home() {
+  return (
+    <main>   
+      <div>
+        <Navbar />
+      </div>
+    
+      <div>
+      <Banner1 />
+      </div> 
+      <div>
+        <Category />
+      </div> 
+      <div>
+         <AllProduct />
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </main> 
+  );
+}
